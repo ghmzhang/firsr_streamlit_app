632d243b87d5bbbe4469a3a08536e3ba0c2358ab
ghmzhang <136487983+ghmzhang@users.noreply.github.com>
2023-06-15 12:09:30 -0600

    Update streamlit_app.py
    change url in request.get

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # qeirwa rhw sR ro axewwn
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'kiwi")
+#streamlit.text(fruityvice_response.json()) # qeirwa rhw sR ro axewwn
 
 # nomalize json response 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
