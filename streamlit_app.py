f5d3bede7fa0ddef7a4a6b1336d893a7aa4cc79b
ghmzhang <136487983+ghmzhang@users.noreply.github.com>
2023-06-15 16:01:10 -0600

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 fruit_add = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('The user entered ', fruit_add)
+streamlit.write('Thanks for adding ', fruit_add)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_add)
-#streamlit.text('Thanks for adding ' + fruit_add) 
+
